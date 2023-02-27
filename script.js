92e03ee94e0c56ab105adb2cf4c27877fa11b319
Johnrave <johnravemimayn@gmail.com>
2023-02-26 20:03:53 -0800

    50%done

---
+++ script.js
@@ -0,0 +1,52 @@
+
+const dark = document.querySelector('#moon')
+
+let darkCheck = localStorage.getItem('dark-theme')
+
+const darkmode = () =>{
+    document.body.classList.add("darkmode");
+    dark.classList.toggle('bi-sun-fill')
+    localStorage.setItem("dark-theme","enable");
+}
+
+const lightmode = () =>{
+    document.body.classList.remove("darkmode");
+    dark.classList.toggle('bi-sun-fill')
+    localStorage.setItem("dark-theme","disable")
+}
+
+if(darkCheck === "enable"){
+    darkmode() //set the dark-theme on load page
+}
+
+dark.addEventListener('click', ()=>{
+ darkCheck = localStorage.getItem('dark-theme') //update the value of darktheme
+
+    if(darkCheck === 'enable'){
+        lightmode()
+    }
+   else{
+    darkmode()
+   }
+})
+
+
+document.querySelector('#Hamburger').addEventListener('click', ()=>{
+    document.querySelector('.links').classList.toggle('left')
+    document.body.classList.toggle("forscroll")
+    document.querySelector("#Hamburger").classList.toggle("bi-x-lg")
+})
+
+document.querySelectorAll("#link").forEach(li => li.addEventListener('click',()=>{
+    document.querySelector('.links').classList.remove('left')
+    document.body.classList.remove("forscroll")
+    document.querySelector("#Hamburger").classList.remove("bi-x-lg")
+}))
+
+
+
+
+
+
+
+
